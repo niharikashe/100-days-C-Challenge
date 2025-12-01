2ed040c6a514ad905bd44577c97408d8fdf5b1d5
niharikashe <niharika2525103@gmail.com>
2025-12-02 02:28:45 +0530

    Update and rename Day 22 to day22.q43.c

--- day22.q43.c
+++ day22.q43.c
@@ -33,28 +33,3 @@ int main() {
     return 0;
 }
 
-Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
-
-#include <stdio.h>
-
-int main() {
-    int n;
-    float sum = 0.0;
-
-    printf("Enter number of terms: ");
-    scanf("%d", &n);
-
-    int numerator = 1;
-    int denominator = 1;
-
-    for (int i = 1; i <= n; i++) {
-        sum += (float)numerator / denominator;
-
-        numerator += 2;    // Next odd number
-        denominator += 2;  // Next even number
-    }
-
-    printf("Approximate sum: %.1f\n", sum);
-
-    return 0;
-}
