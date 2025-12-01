6fd38f86705a63871684b584ac2f96377eb6c41b
niharikashe <niharika2525103@gmail.com>
2025-12-01 22:46:59 +0530

    Create day03.q6.c

---
+++ day03.q6.c
@@ -0,0 +1,19 @@
+Q6: Write a program to swap two numbers using a third variable.
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // swapping using temp
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swap: %d %d\n", a, b);
+
+    return 0;
+}
