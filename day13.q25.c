c248c917fd73ef96124cedc329f10c89ffc94270
niharikashe <niharika2525103@gmail.com>
2025-12-02 02:04:31 +0530

    Update and rename Day 13 to day13.q25.c

--- day13.q25.c
+++ day13.q25.c
@@ -42,19 +42,3 @@ int main() {
     return 0;
 }
 
-Q26: Write a program to print numbers from 1 to n.
-
-#include <stdio.h>
-
-int main() {
-    int n, i;
-
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    for (i = 1; i <= n; i++) {
-        printf("%d ", i);
-    }
-
-    return 0;
-}
