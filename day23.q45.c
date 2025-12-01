e08d402cff6267c1a87560792984bd07d51df202
niharikashe <niharika2525103@gmail.com>
2025-12-02 02:34:27 +0530

    Update and rename Day 23 to day23.q45.c

--- day23.q45.c
+++ day23.q45.c
@@ -21,24 +21,3 @@ int main() {
     return 0;
 }
 
-Q46: Write a program to print the following pattern:
-*****
-*****
-*****
-*****
-*****
-
-#include <stdio.h>
-
-int main() {
-    int i, j;
-
-    for (i = 1; i <= 5; i++) {       
-        for (j = 1; j <= 5; j++) {   
-            printf("*");
-        }
-        printf("\n");               
-    }
-
-    return 0;
-}
