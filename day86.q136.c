347234518dd5a08c3501bfd983b9b2bdee012785
niharikashe <niharika2525103@gmail.com>
2025-12-02 11:54:17 +0530

    Update and rename Day 86 to day86.q136.c

--- day86.q136.c
+++ day86.q136.c
@@ -1,4 +1,4 @@
-Use enum to represent menu choices (ADD, SUBTRACT, MULTIPLY) and perform operations using switch.
+Q136: Use enum to represent menu choices (ADD, SUBTRACT, MULTIPLY) and perform operations using switch.
 #include <stdio.h>
 
 enum Menu {
